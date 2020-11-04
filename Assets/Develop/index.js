434633a3073818a7776accf726716549e3da15fd
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-11-04 11:29:31 -0500

    Finished the questions array for now. Created a new array with license options for the user to choose from. Will start creating the prompts for the list of license options.

--- Assets/Develop/index.js
+++ Assets/Develop/index.js
@@ -1,10 +1,55 @@
 console.log("Is thing on?");
 const inquirer = require("inquirer");
-inquirer.prompt([questions])
+inquirer.prompt([
+    {
+        type: "input",
+        message: questions[0],
+        name: "projectTitle"
+    },
+    {
+        type: "input",
+        message: questions[1],
+        name: "projectDescription"
+    },
+    {
+        type: "input",
+        message: questions[2],
+        name: "projectInstallation"
+    },
+    {
+        type: "input",
+        message: questions[3],
+        name: "projectUsage"
+    },
+    {
+        type: "input",
+        message: questions[4],
+        name: "projectContribution"
+    },
+    {
+        type: "input",
+        message: questions[5],
+        name: "projectTesting"
+    },
+    {
+        type: "input",
+        message: questions[7],
+        name: "Github"
+    },
+    {
+        type: "input",
+        message: questions[8],
+        name: "emailAddress"
+    },
+])
 
 // array of questions for user
 const questions = [
-    "What is the name/title of your project?", "What can you tell me about your project? Write a brief description (no more than 2 paragraphs)", "How would a user install your project? Write step-by-step instructions.", "How should a client properly use this product?", "How can I, another developer, contribute to this project? Please write clear and concise guidelines.", "How can I test this application? Please write clear and concise instructions.", "How can you, the website author, be contacted in case of questions?"
+    "What is the name/title of your project?", "What can you tell me about your project? Write a brief description (no more than 2 paragraphs)", "How would a user install your project? Write step-by-step instructions.", "How should a client properly use this product?", "How can I, another developer, contribute to this project? Please write clear and concise guidelines.", "How can I test this application? Please write clear and concise instructions.", "What is your Github username?", "What is the best email address for users to contact you with questions?"
+];
+// array of licenses for user to choose from
+const licenseOptions = [
+    
 ];
 
 // function to write README file
