c5a964d781acd9450f575ec892ff4af555fac46c
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-11-04 11:06:58 -0500

    Added my questions to the questions array to get info from the website author for the readme.

--- Assets/Develop/index.js
+++ Assets/Develop/index.js
@@ -1,7 +1,10 @@
 console.log("Is thing on?");
+const inquirer = require("inquirer");
+inquirer.prompt([questions])
+
 // array of questions for user
 const questions = [
-
+    "What is the name/title of your project?", "What can you tell me about your project? Write a brief description (no more than 2 paragraphs)", "How would a user install your project? Write step-by-step instructions.", "How should a client properly use this product?", "How can I, another developer, contribute to this project? Please write clear and concise guidelines.", "How can I test this application? Please write clear and concise instructions.", "How can you, the website author, be contacted in case of questions?"
 ];
 
 // function to write README file
